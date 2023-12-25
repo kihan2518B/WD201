b04b142286aad64b2819623ddd86011b20af2362
kihan2518B <kp648027@gmail.com>
2023-12-25 18:36:39 +0530

    added last precommit

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,51 +1,50 @@
-const express = require('express')
-const app = express()
-const { Todo } = require('./models')
+const express = require("express");
+const app = express();
+const { Todo } = require("./models");
 // const { bodyParser } = require('body-parser')
-app.use(express.json())
+app.use(express.json());
 
 app.get("/todos", async (request, response) => {
-    // response.send('hello world')
-    const todoItems = await Todo.gettodo();
-    response.json(todoItems)
-})
+  // response.send('hello world')
+  const todoItems = await Todo.gettodo();
+  response.json(todoItems);
+});
 
 app.post("/todos", async (request, response) => {
-    // console.log('Created Todo', request.body)
-    try {
-        const todo = await Todo.addtodo({
-            title: request.body.title,
-            dueDate: request.body.dueDate,
-            completed: false
-        })
-        return response.json(todo)
-    } catch (error) {
-        console.log(error)
-        return response.status(422).json(error)
-    }
-
-})
+  // console.log('Created Todo', request.body)
+  try {
+    const todo = await Todo.addtodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+      completed: false,
+    });
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
 
 //https://mttodoapp.com/todos/123/markascompleted
 app.put("/todos/:id/markascompleted", async (request, response) => {
-    // console.log('Updated Todo with id', request.params.id)
-    const todo = await Todo.findByPk(request.params.id)
-    try {
-        const UpdatedTodo = await todo.markAsCompleted()
-        return response.json(UpdatedTodo)
-    } catch (error) {
-        console.log(error)
-        return response.status(422).json(error)
-    }
-})
+  // console.log('Updated Todo with id', request.params.id)
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const UpdatedTodo = await todo.markAsCompleted();
+    return response.json(UpdatedTodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
 
 app.delete("/todos/:id", (request, response) => {
-    console.log('Item Deleted with id', request.params.id)
-})
+  console.log("Item Deleted with id", request.params.id);
+});
 app.listen(3000, () => {
-    try {
-        console.log('server is running on port 3000')
-    } catch (err) {
-        console.log('error while running server')
-    }
-})
+  try {
+    console.log("server is running on port 3000");
+  } catch (err) {
+    console.log("error while running server");
+  }
+});
--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -13,19 +11,22 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
     static addtodo({ title, dueDate }) {
-      return this.create({ title: title, dueDate: dueDate, completed: false })
+      return this.create({ title: title, dueDate: dueDate, completed: false });
     }
     markAsCompleted() {
-      return this.update({ completed: true })
+      return this.update({ completed: true });
     }
   }
-  Todo.init({
-    title: DataTypes.STRING,
-    dueDate: DataTypes.DATEONLY,
-    completed: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Todo',
-  });
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    },
+  );
   return Todo;
 };
